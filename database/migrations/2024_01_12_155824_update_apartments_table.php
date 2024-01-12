7fe6bf931bef0b982a0e9c892d6b27c42be0a8bc
VeronicaFontana <fontana.veronica98@gmail.com>
2024-01-12 18:09:18 +0100

    [fix] drop foreign key in apartments table

--- database/migrations/2024_01_12_155824_update_apartments_table.php
+++ database/migrations/2024_01_12_155824_update_apartments_table.php
@@ -24,7 +24,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('apartments', function (Blueprint $table) {
-            $table->dropForeign('user_id');
+            $table->dropForeign(['user_id']);
             $table->dropColumn('user_id');
         });
     }
