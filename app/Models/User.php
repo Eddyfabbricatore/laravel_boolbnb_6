d13e4363bb79c1aebd60f06e5cd8962f2bbe7e9d
Eddyfabbricatore <eddyfabbricatore2@gmail.com>
2024-01-12 17:10:56 +0100

    [add] relation one to many(user has many apartments)

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use App\Models\Apartment;
 
 class User extends Authenticatable
 {
@@ -24,6 +25,10 @@ class User extends Authenticatable
         'password',
         'date_of_birth'
     ];
+
+    public function apartments(){
+        return $this->hasMany(Apartment::class);
+    }
 
     /**
      * The attributes that should be hidden for serialization.
