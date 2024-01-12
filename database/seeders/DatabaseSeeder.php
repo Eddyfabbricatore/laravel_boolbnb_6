d68ccceff2f20467d164f6c55fcfff935c0b3657
VeronicaFontana <fontana.veronica98@gmail.com>
2024-01-12 18:12:47 +0100

    [add] apartments seeder in DB seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,7 +13,8 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call([
-            UserTableSeeder::class
+            UserTableSeeder::class,
+            ApartmentsTableSeeder::class
         ]);
     }
 }
