750900be516c76362f7a56fe1b34d6a2d0cc4315
vincenzoimmordino92 <vincenzoimmordino92@gmail.com>
2024-01-13 18:42:00 +0100

    [fix] service_id seed error in apartment service table

--- database/migrations/2024_01_13_160057_create_apartment_service_table.php
+++ database/migrations/2024_01_13_160057_create_apartment_service_table.php
@@ -12,13 +12,13 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('apartment_service', function (Blueprint $table) {
-            $table->unsignedBigInteger('apartment_id');
+            $table->unsignedBigInteger('apartment_id')->nullable();
             $table->foreign('apartment_id')
             ->references('id')
             ->on('apartments')
             ->cascadeOnDelete();
 
-            $table->unsignedBigInteger('service_id');
+            $table->unsignedBigInteger('service_id')->nullable();
             $table->foreign('service_id')
             ->references('id')
             ->on('services')
