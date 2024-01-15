e09ed8dfb82951928da7e608316300957ffa30e3
alessandrodaniello105 <kanymsevement@gmail.com>
2024-01-15 14:38:11 +0100

    [add] links in header navbar (front home + back dashboard)

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -33,7 +33,10 @@
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav me-auto">
                         <li class="nav-item">
-                            <a class="nav-link" href="{{url('/') }}">{{ __('Home') }}</a>
+                            <a class="nav-link" href="{{url('/') }}">{{ __('Vai al sito') }}</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{route('dashboard')}}">{{ __('Dashboard') }}</a>
                         </li>
                     </ul>
 
