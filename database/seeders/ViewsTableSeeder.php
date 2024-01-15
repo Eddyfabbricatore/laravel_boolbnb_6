5c075d4a3c6364e46dbca8941eb20d8be406c8dc
Pirisinu <palazzialessandro2@gmail.com>
2024-01-15 17:10:32 +0100

    [ add ] Views (migration + seeder)

---
+++ database/seeders/ViewsTableSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Apartment;
+use App\Models\View;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+
+class ViewsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(Faker $faker): void
+    {
+
+
+        for($i = 0; $i < 50 ; $i++){
+
+            $newView = new View();
+            $newView->apartment_id = Apartment::inRandomOrder()->first()->id;
+            $newView->ip = $faker->ipv4();
+            $newView->view_date = $faker->dateTime();
+
+            $newView->save();
+        }
+    }
+
+}
