7fca13b82ba87cda72dde0e0b7387f0a17020f5c
Pirisinu <palazzialessandro2@gmail.com>
2024-01-15 13:06:21 +0100

    [ edit ] view (app,dashboard)

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,26 +1,18 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <h2 class="fs-4 text-secondary my-4">
-        {{ __('Dashboard') }}
-    </h2>
-    <div class="row justify-content-center">
-        <div class="col">
-            <div class="card">
-                <div class="card-header">{{ __('User Dashboard') }}</div>
 
-                <div class="card-body">
-                    @if (session('status'))
-                    <div class="alert alert-success" role="alert">
-                        {{ session('status') }}
-                    </div>
-                    @endif
+    <ul>
+        @foreach ($apartments as $apartment)
+            <li>
+                {{$apartment->title}}
+                @forelse ($apartment->services as $service)
+                    <span class="badge text-bg-info">{{ $service->name }}</span>
+                @empty
+                    -
+                @endforelse
+            </li>
+        @endforeach
+    </ul>
 
-                    {{ __('You are logged in!') }}
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
 @endsection
