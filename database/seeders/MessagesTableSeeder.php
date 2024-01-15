824de3ffce1c65235beed91d4d2d4888d9d82061
Pirisinu <palazzialessandro2@gmail.com>
2024-01-15 17:08:34 +0100

    [ add ] Message (migration + seeder)

---
+++ database/seeders/MessagesTableSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Apartment;
+use App\Models\Message;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+
+class MessagesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(Faker $faker): void
+    {
+
+        for($i = 0; $i < 20 ; $i++) {
+
+            $newMessage = new Message();
+            $newMessage->apartment_id = Apartment::inRandomOrder()->first()->id;
+            $newMessage->full_name = $faker->name();
+            $newMessage->email = $faker->email();
+            $newMessage->message = $faker->sentence();
+            $newMessage->date = $faker->dateTimeBetween('-4 week', 'yesterday');
+
+            $newMessage->save();
+        }
+    }
+
+}
