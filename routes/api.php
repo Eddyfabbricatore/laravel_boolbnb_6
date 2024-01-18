d074cd5398f0cc57a1913ef0dc609756b3d9fc62
vincenzoimmordino92 <vincenzoimmordino92@gmail.com>
2024-01-18 11:04:29 +0100

    [add] router test search

--- routes/api.php
+++ routes/api.php
@@ -20,3 +20,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::get('/apartments', [ApartmentController::class, 'getApartments']);
+Route::get('/search', [ApartmentController::class, 'viewApartamentsInSearchAdvance']);
