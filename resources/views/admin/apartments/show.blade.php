9d5fd765fcf9fdd28b7ec16788f78ba24a1a3cd4
alessandrodaniello105 <kanymsevement@gmail.com>
2024-01-18 11:30:08 +0100

    [add]: actions buttons in apartment show

--- resources/views/admin/apartments/show.blade.php
+++ resources/views/admin/apartments/show.blade.php
@@ -8,9 +8,23 @@
         }
     </style>
     <section class="container w-100 h-100">
-        <h1>Il tuo appartamento</h1>
 
         <div class="card mb-3">
+            <div class="d-flex gap-5 align-items-center ">
+                <h1 class="text-dark ">Il tuo appartamento</h1>
+
+                <form action={{route("admin.apartments.destroy", $apartment)}} method="post" onsubmit="return confirm('Are you sure you want to delete this apartment?')">
+                    <button class="btn btn-warning">
+                        <a class="nav-link btn btn-warning" href="{{route('admin.apartments.edit', $apartment->id)}}"><i class="fa-sharp fa-solid fa-pen"></i></a>
+                    </button>
+                    @csrf
+                    @method('DELETE')
+                    <button type="submit" class="btn btn-danger">
+                        <i class="fa-solid fa-trash fa-bounce"></i>
+                    </button>
+                </form>
+            </div>
+
             <!-- <img src="{{asset('storage/'.$apartment->image)}}" onerror="this.src='/img/Placeholder.png'" class="card-img-top" alt="{{$apartment->title}}"> -->
             <div class="card-body">
                 <h5 class="card-title">{{ $apartment->title }}</h5>
