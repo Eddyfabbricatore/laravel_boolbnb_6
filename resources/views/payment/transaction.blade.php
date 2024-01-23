553211e5afcfe656e6b9fe5b3af209816f039894
Pirisinu <palazzialessandro2@gmail.com>
2024-01-23 15:19:50 +0100

    [ edit ] transiction complete view

--- resources/views/payment/transaction.blade.php
+++ resources/views/payment/transaction.blade.php
@@ -1,5 +1,27 @@
 @extends('layouts.app')
 
 @section('content')
-transizione
+    <div>
+        <h1>Payment Result</h1>
+
+        @if(session('success'))
+            <div style="color: green;">
+                {{ session('success') }}
+            </div>
+        @elseif(session('errors'))
+            <div style="color: red;">
+                {{ session('errors') }}
+            </div>
+        @endif
+
+        @isset($transaction)
+            <h2>Transaction Details:</h2>
+            <ul>
+                <li>ID: {{ $transaction }}</li>
+                <li>Amount: {{ $transaction->amount }}</li>
+                <li>Status: {{ $transaction->status }}</li>
+                <!-- Add more transaction details as needed -->
+            </ul>
+        @endisset
+    </div>
 @endsection
