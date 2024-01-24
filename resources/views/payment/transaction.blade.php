9f1b219c5a7f6540af7148eca3ddaa34ed991a46
Eddyfabbricatore <eddyfabbricatore2@gmail.com>
2024-01-24 13:07:08 +0100

    [add] setInterval(), tempo rimanente (prova)

--- resources/views/payment/transaction.blade.php
+++ resources/views/payment/transaction.blade.php
@@ -21,9 +21,22 @@
                 <li>Amount: {{ $transaction->amount }}</li>
                 <li>Status: {{ $transaction->status }}</li>
                 <li>Data di transizione: {{ $transaction->createdAt->format('Y-m-d H:i:s') }}</li>
+                <li>Durata sponsorizzazione in ore: {{ $sponsor_duration }}</li>
+                <li>{{ $isSponsored }}</li>
+                <li>Tempo rimanente: <span id="tempo-rimanente">{{ $tempoRimanente }}</span> secondi</li>
                 <!-- Add more transaction details as needed -->
             </ul>
         </div>
         @endisset
     </div>
+
+    <script>
+        setInterval(function() {
+        var tempoRimanente = parseInt(document.getElementById('tempo-rimanente').innerText);
+        if (tempoRimanente > 0) {
+            tempoRimanente--;
+            document.getElementById('tempo-rimanente').innerText = tempoRimanente;
+        }
+    }, 1000);
+    </script>
 @endsection
