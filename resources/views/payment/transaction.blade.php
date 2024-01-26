62dc05d0c5d8cdb5adeb9cd9631fad16d6eedeed
Eddyfabbricatore <eddyfabbricatore2@gmail.com>
2024-01-26 13:39:47 +0100

    [edit] style payment transaction

--- resources/views/payment/transaction.blade.php
+++ resources/views/payment/transaction.blade.php
@@ -1,34 +1,36 @@
 @extends('layouts.app')
 
 @section('content')
-    <div>
+    @if(session('success'))
+        <div style="color: green;">
+            {{ session('success') }}
+        </div>
+    @elseif(session('errors'))
+        <div style="color: red;">
+            {{ session('errors') }}
+        </div>
+    @endif
+    <div class="container">
         <h1>Payment Result</h1>
-
-        @if(session('success'))
-            <div style="color: green;">
-                {{ session('success') }}
-            </div>
-        @elseif(session('errors'))
-            <div style="color: red;">
-                {{ session('errors') }}
-            </div>
-        @endif
-
 
         @isset($transaction)
         <div class="card">
-            <h2>Transaction Details:</h2>
-            <ul>
-                <li>Sponsorizzato: <span id="sponsored">{{ $apartment->sponsorizzato }}</span></li>
-                <li>Status: {{ $transaction }}</li>
-                <li>Data di transizione: {{ $transaction->createdAt->format('Y-m-d H:i:s') }}</li>
-                <li>Durata sponsorizzazione in ore: {{ $sponsor_duration }}</li>
-                <li>{{ $isSponsored }}</li>
-                <li>Tempo rimanente: <span id="tempo-rimanente">{{ $newEndDate }}</span> secondi</li>
-            </ul>
+            <h2>Riepilogo transazione</h2>
+
+            <!-- <div style="
+    background-image: url('@if(file_exists(public_path('storage/' . $apartment->image) )){{ asset('storage/' . $apartment->image) }}@else/img/{{$apartment->image}}@endif');
+    background-size: cover;"> -->
+            <div style="background-image: url(public_path('storage/' . $apartment->image));">
+                <p>Hai sponsorizzato: <span id="sponsored">{{ $apartment->title }}</span> con l'abbonamento {{$sponsor->name}}</p>
+                <p>Status: {{ $transaction }}</p>
+                <p>Data di transizione: {{ $transaction->createdAt->format('Y-m-d H:i:s') }}</p>
+                <p>{{ $isSponsored }}</p>
+                <p>Durata sponsorizzazione in ore: {{ $sponsor_duration }}</p>
+                <!-- <p id="tempo-rimanente">Rimangono {{ $newEndDate }} secondi alla fine della sponsorizzazione</p> -->
+            </div>
         </div>
-        <div>
-            <a class="btn btn-secondary" href="{{route('admin.apartments.index', $apartment)}}">Torna alla Lista</a>
+        <div class="mt-3">
+            <a class="btn btn-secondary" href="{{route('admin.apartments.show', $apartment)}}">Torna all'appartamento</a>
         </div>
         @endisset
     </div>
