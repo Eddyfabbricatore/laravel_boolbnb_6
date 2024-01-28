399f66e7fb6262198a21cac3db44f5c4d9635668
Pirisinu <palazzialessandro2@gmail.com>
2024-01-28 18:09:46 +0100

    [ add ] StatsController::index()

--- app/Http/Controllers/Admin/StatsController.php
+++ app/Http/Controllers/Admin/StatsController.php
@@ -3,9 +3,17 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Apartment;
+use App\Models\View;
 use Illuminate\Http\Request;
 
 class StatsController extends Controller
 {
-    //
+    public function index(Apartment $apartment){
+        $statsView = View::selectRaw('COUNT(*) as count, DATE(view_date) as date')
+                        ->groupBy('date')
+                        ->get();
+
+        return view('admin.stats.index', compact('apartment', 'statsView'));
+    }
 }
