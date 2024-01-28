dc7ceb8caba848e7d38276def3b58cd83030ef93
Pirisinu <palazzialessandro2@gmail.com>
2024-01-28 15:44:24 +0100

    [ edit ] dashboard style color

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -11,8 +11,8 @@
     @endphp
     @dump($_SESSION); --}}
 
-    <div id="dashboard" class="container">
-        <div class="route-select border rounded-3 m-auto mt-5 p-5 w-100 d-flex flex-column">
+    <div id="dashboard" class="container h-100 d-flex align-items-center">
+        <div class="route-select border rounded-3 m-auto p-5 h-75 w-100 d-flex align-items-center justify-content-center flex-column">
             <h1 class="text-center">Bentornato {{$user->name}} {{$user->surname}}!!</h1>
 
             <h3>Scegli cosa fare:</h3>
@@ -26,9 +26,9 @@
                     </a>
                 </li>
                 <li class="h-100 flex-grow-1">
-                    <a  href="{{route('admin.apartments.index')}}">
-                        <div class="icon border rounded-5">
-                            <div class="d-flex gap-2">
+                    <a class="w-100"  href="{{route('admin.apartments.index')}}">
+                        <div class="icon border rounded-5 w-100">
+                            <div class="d-flex justify-content-center gap-2 w-100">
                                 <i class="fa-solid fa-list"></i>
                                 <i class="fa-solid fa-house"></i>
                             </div>
