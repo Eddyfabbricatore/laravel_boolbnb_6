f85a19fb83bd0b9892ccd8421cb2da15a96e83eb
VeronicaFontana <fontana.veronica98@gmail.com>
2024-01-30 13:11:20 +0100

    [add] No message received condition in messages blade

--- resources/views/admin/apartments/messages.blade.php
+++ resources/views/admin/apartments/messages.blade.php
@@ -7,23 +7,28 @@
 
         <div id="show-form" class="card p-5">
 
-            @foreach ($messages as $message)
-                <div class="media-body u-shadow-v18 g-pa-30 mb-2 w-75">
-                    <div class="g-mb-15">
-                        <h5 class="h5 g-color-gray-dark-v1 mb-0">{{ $message->full_name }}</h5>
-                        <span class="g-color-gray-dark-v4 g-font-size-12">{{ \Carbon\Carbon::parse($message->date)->locale('it')->isoFormat('DD MMMM YYYY') }}</span>
+            @if (count($messages) == 0)
+                <h5>Non hai ancora ricevuto messaggi per questo appartamento</h5>
+            @else
+                @foreach ($messages as $message)
+                    <div class="media-body u-shadow-v18 g-pa-30 mb-2 w-75">
+                        <div class="g-mb-15">
+                            <h5 class="h5 g-color-gray-dark-v1 mb-0">{{ $message->full_name }}</h5>
+                            <span class="g-color-gray-dark-v4 g-font-size-12">{{ \Carbon\Carbon::parse($message->date)->locale('it')->isoFormat('DD MMMM YYYY') }}</span>
+                        </div>
+                        <p>{{ $message->message }}</p>
+                        <ul class="list-inline d-sm-flex my-0">
+                            <li class="list-inline-item">
+                                <a class="u-link-v5 g-color-gray-dark-v4 g-color-primary--hover" href="mailto:{{ $message->email }}">
+                                    <i class="fa fa-reply g-pos-rel g-top-1 g-mr-3"></i>
+                                    Reply
+                                </a>
+                            </li>
+                        </ul>
                     </div>
-                    <p>{{ $message->message }}</p>
-                    <ul class="list-inline d-sm-flex my-0">
-                        <li class="list-inline-item">
-                            <a class="u-link-v5 g-color-gray-dark-v4 g-color-primary--hover" href="mailto:{{ $message->email }}">
-                                <i class="fa fa-reply g-pos-rel g-top-1 g-mr-3"></i>
-                                Reply
-                            </a>
-                        </li>
-                    </ul>
-                </div>
-            @endforeach
+                @endforeach
+            @endif
+
 
         </div>
     </section>
