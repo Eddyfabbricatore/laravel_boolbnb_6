552e79d58ccb8ee58ee75f2977d9eb6eb9571a9c
VeronicaFontana <fontana.veronica98@gmail.com>
2024-01-30 13:05:55 +0100

    [edit] Messages body

--- resources/views/admin/apartments/messages.blade.php
+++ resources/views/admin/apartments/messages.blade.php
@@ -6,22 +6,25 @@
         <h1 class="text-black">I tuoi messaggi</h1>
 
         <div id="show-form" class="card p-5">
-            <div class="d-flex flex-wrap gap-5 align-items-center ">
 
-                @foreach ($messages as $message)
-                    <div class="list-group">
-                        <a class="list-group-item">
-                            <div class="w-100">
-                                <h5 class="mb-1">{{ $message->full_name }}</h5>
-                                <small class="text-secondary">{{ \Carbon\Carbon::parse($message->date)->locale('it')->isoFormat('DD MMMM YYYY') }}</small>
-                            </div>
-                            <p class="mb-1"><em>{{ $message->email }}</em></p>
-                            <small>{{ $message->message }}</small>
-                        </a>
+            @foreach ($messages as $message)
+                <div class="media-body u-shadow-v18 g-pa-30 mb-2 w-75">
+                    <div class="g-mb-15">
+                        <h5 class="h5 g-color-gray-dark-v1 mb-0">{{ $message->full_name }}</h5>
+                        <span class="g-color-gray-dark-v4 g-font-size-12">{{ \Carbon\Carbon::parse($message->date)->locale('it')->isoFormat('DD MMMM YYYY') }}</span>
                     </div>
-                @endforeach
+                    <p>{{ $message->message }}</p>
+                    <ul class="list-inline d-sm-flex my-0">
+                        <li class="list-inline-item">
+                            <a class="u-link-v5 g-color-gray-dark-v4 g-color-primary--hover" href="mailto:{{ $message->email }}">
+                                <i class="fa fa-reply g-pos-rel g-top-1 g-mr-3"></i>
+                                Reply
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            @endforeach
 
-            </div>
         </div>
     </section>
 @endsection
