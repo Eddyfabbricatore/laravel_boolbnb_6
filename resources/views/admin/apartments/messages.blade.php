2694eb2159034f1f4bd8b21de302c06759c82b0b
VeronicaFontana <fontana.veronica98@gmail.com>
2024-01-30 10:25:23 +0100

    [add] Message card in messages blade

--- resources/views/admin/apartments/messages.blade.php
+++ resources/views/admin/apartments/messages.blade.php
@@ -2,23 +2,23 @@
 
 @section('content')
 
-    <section class="container w-100 h-100 d-flex justify-content-center align-items-center">
+    <section class="container w-100 h-100 py-3">
+        <h1>I tuoi messaggi</h1>
 
         <div id="show-form" class="card p-5">
             <div class="d-flex gap-5 align-items-center ">
-                <h1 class="text-dark ">
+
                     @foreach ($messages as $message)
-                        <p>{{ $message->full_name }}</p>
-                        <p>{{ $message->email }}</p>
-                        <p>{{ $message->message }}</p>
-                        <!-- Altri attributi del messaggio -->
+                        <div class="card" style="width: 18rem;">
+                            <div class="card-body">
+                                <h5 class="card-title">{{ $message->full_name }}</h5>
+                                <h6 class="card-subtitle mb-2 text-body-secondary">{{ $message->email }}</h6>
+                                <p class="card-text">{{ $message->message }}</p>
+                            </div>
+                        </div>
                     @endforeach
 
-                </h1>
-
             </div>
-
-
         </div>
     </section>
 @endsection
