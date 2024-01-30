43d1988a829c26e396b6e6f87d00eddb6617fbff
VeronicaFontana <fontana.veronica98@gmail.com>
2024-01-30 11:21:22 +0100

    [add] Messages list element

--- resources/views/admin/apartments/messages.blade.php
+++ resources/views/admin/apartments/messages.blade.php
@@ -3,20 +3,23 @@
 @section('content')
 
     <section class="container w-100 h-100 py-3">
-        <h1>I tuoi messaggi</h1>
+        <h1 class="text-black">I tuoi messaggi</h1>
 
         <div id="show-form" class="card p-5">
-            <div class="d-flex gap-5 align-items-center ">
+            <div class="d-flex flex-wrap gap-5 align-items-center ">
 
-                    @foreach ($messages as $message)
-                        <div class="card" style="width: 18rem;">
-                            <div class="card-body">
-                                <h5 class="card-title">{{ $message->full_name }}</h5>
-                                <h6 class="card-subtitle mb-2 text-body-secondary">{{ $message->email }}</h6>
-                                <p class="card-text">{{ $message->message }}</p>
+                @foreach ($messages as $message)
+                    <div class="list-group">
+                        <a class="list-group-item">
+                            <div class="w-100">
+                                <h5 class="mb-1">{{ $message->full_name }}</h5>
+                                <small class="text-secondary">{{ \Carbon\Carbon::parse($message->date)->locale('it')->isoFormat('DD MMMM YYYY') }}</small>
                             </div>
-                        </div>
-                    @endforeach
+                            <p class="mb-1"><em>{{ $message->email }}</em></p>
+                            <small>{{ $message->message }}</small>
+                        </a>
+                    </div>
+                @endforeach
 
             </div>
         </div>
