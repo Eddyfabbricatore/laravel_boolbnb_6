28269adb37c2f76f6d70b5ec0e95cc9ddd64f58c
Pirisinu <palazzialessandro2@gmail.com>
2024-01-30 14:57:21 +0100

    [ edit ] apartment_sponsor seeder

--- database/seeders/ApartmentSponsorTableSeeder.php
+++ database/seeders/ApartmentSponsorTableSeeder.php
@@ -6,21 +6,29 @@ use App\Models\Apartment;
 use App\Models\Sponsor;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
 
 class ApartmentSponsorTableSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
-    public function run(): void
+    public function run(Faker $faker): void
     {
-        for($i = 0; $i < 24 ;$i++){
+        for($i = 0; $i < 750 ;$i++){
 
             $apartment = Apartment::inRandomOrder()->first();
 
             $sponsor = Sponsor::inRandomOrder()->first();
 
-            $apartment->sponsors()->attach($sponsor->id);
+            $transactionDate = $faker->dateTimeBetween('-2 years', 'yesterday');
+
+            $endSponsorDate = $faker->dateTimeBetween('-2 years', 'yesterday');
+
+            $apartment->sponsors()->attach($sponsor, [
+                'transaction_date' => $transactionDate,
+                'end_sponsor_date' => $endSponsorDate,
+            ]);
 
         }
     }
