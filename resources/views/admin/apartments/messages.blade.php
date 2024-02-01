ba2327e238541db1dbe2be8eddbf5595bc9b4361
alessandrodaniello105 <kanymsevement@gmail.com>
2024-02-01 01:34:10 +0100

    [add] paginator in message blade

--- resources/views/admin/apartments/messages.blade.php
+++ resources/views/admin/apartments/messages.blade.php
@@ -13,23 +13,32 @@
             @if (count($messages) == 0)
                 <h5>Non hai ancora ricevuto messaggi per questo appartamento</h5>
             @else
+
+
+            {{$messages->links()}}
+            <div class="container">
                 @foreach ($messages as $message)
-                    <div class="media-body u-shadow-v18 g-pa-30 mb-2 w-75">
-                        <div class="g-mb-15">
-                            <h5 class="h5 g-color-gray-dark-v1 mb-0">{{ $message->full_name }}</h5>
-                            <span class="g-color-gray-dark-v4 g-font-size-12">{{ \Carbon\Carbon::parse($message->date)->locale('it')->isoFormat('DD MMMM YYYY') }}</span>
-                        </div>
-                        <p>{{ $message->message }}</p>
-                        <ul class="list-inline d-sm-flex my-0">
-                            <li class="list-inline-item">
-                                <a class="u-link-v5 g-color-gray-dark-v4 g-color-primary--hover" href="mailto:{{ $message->email }}">
-                                    <i class="fa fa-reply g-pos-rel g-top-1 g-mr-3"></i>
-                                    Reply
-                                </a>
-                            </li>
-                        </ul>
+                <div class="media-body u-shadow-v18 g-pa-30 mb-2 text-center mx-auto">
+                    <div class="g-mb-15">
+                        <h5 class="h5 g-color-gray-dark-v1 mb-2 mb-0">{{ $message->full_name }}</h5>
+                        <span class="g-color-gray-dark-v4 g-font-size-12">{{ \Carbon\Carbon::parse($message->date)->locale('it')->isoFormat('DD MMMM YYYY') }}</span>
                     </div>
-                @endforeach
+                    <p class="mt-4">{{ $message->message }}</p>
+                    <ul class="list-inline d-sm-flex my-0 float-end">
+                        <li class="list-inline-item">
+                            <a class="u-link-v5 g-color-gray-dark-v4 g-color-primary--hover" href="mailto:{{ $message->email }}">
+                                <i class="fa fa-reply g-pos-rel g-top-1 g-mr-3"></i>
+                                Reply
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            @endforeach
+            </div>
+            {{$messages->links()}}
+
+
+
             @endif
 
 
